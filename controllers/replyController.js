99ab82f8893ebd8520133f0e671def3c6dd255c5
yanisgoncam-coder <yanisgoncam@gmail.com>
2025-10-21 16:48:39 -0300

    Update replyController.js

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -59,7 +59,7 @@ class ReplyController {
         throw new Error('Reply not found');
       }
 
-      return 'success';
+      return 'reported';
     } catch (error) {
       throw new Error('Error reporting reply: ' + error.message);
     }
